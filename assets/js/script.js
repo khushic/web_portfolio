22ca46e9ad5e3840572367934754185867910219
Khushi Chawla <emailkhushic@gmail.com>
2021-03-18 15:20:44 -0400

    simplicity catering

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(e) {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 150) {
-        $('.my-navbar').addClass("navbar-hide");
-    } else {
-        $('.my-navbar').removeClass("navbar-hide");
-    }
-});
